084e22306f6872beffe23c1f61115eb3b2cb34a5
Jake Demarest-Mays <jake@demarest-mays.com>
2020-11-13 00:24:52 -0700

    One more try

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,7 @@ const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
 	.use(
+		'jake.demarest-mays.com',
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		sapper.middleware()
