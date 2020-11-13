64dffadeabb837173eb6c151d2654d52d33b9e24
Jake Demarest-Mays <jake@demarest-mays.com>
2020-11-13 00:23:58 -0700

    hooey

--- scripts/gh-pages.js
+++ scripts/gh-pages.js
@@ -1,4 +1,15 @@
-var ghpages = require('gh-pages')
+const { spawnSync } = require('child_process')
+const fs = require('fs')
+const ghpages = require('gh-pages')
+
+const args = process.argv.slice(2)
+if (!args.includes('--built')) {
+  spawnSync('npm', ['run', 'export'], {stdio: 'inherit'})
+}
+
+// This needs to run *after* export.
+fs.writeFileSync(`${__dirname}/../__sapper__/export/bravely.github.io/CNAME`, 'jake.demarest-mays.com')
+console.log('Copied over CName')
 
 ghpages.publish(
   '__sapper__/export/bravely.github.io',
@@ -18,3 +29,4 @@ ghpages.publish(
     }
   }
 )
+
